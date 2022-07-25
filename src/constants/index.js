5add5698c44ebd7da2100eedfcea32f00436f752
Emre Kılıç <emrekilic87@gmail.com>
2022-07-25 22:37:13 +0300

    ssh request problem has been solved

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const endpoints = {
   apikey: process.env.VUE_APP_API_KEY,
-  geo: "http://api.openweathermap.org/geo/1.0",
+  geo: "https://api.openweathermap.org/geo/1.0",
   allData: "https://api.openweathermap.org/data/2.5/onecall",
 };
